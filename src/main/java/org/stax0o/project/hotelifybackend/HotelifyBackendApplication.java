954b1d598e12968ccecd1060189e8e5c0635aa41
Artem Aliev <stax0o.devr@gmail.com>
2025-02-10 10:52:37 +0500

    Init

---
+++ src/main/java/org/stax0o/project/hotelifybackend/HotelifyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.stax0o.project.hotelifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelifyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelifyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/stax0o/project/hotelifybackend/HotelifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stax0o.project.hotelifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
