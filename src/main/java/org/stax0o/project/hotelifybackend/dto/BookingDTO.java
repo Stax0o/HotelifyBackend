bfe8762fdd7a845816f2f60e1e43abd438fbd786
Artem Aliev <stax0o.devr@gmail.com>
2025-02-14 11:49:42 +0500

    feat: Adding restrictions on data added to the database

--- src/main/java/org/stax0o/project/hotelifybackend/dto/BookingDTO.java
+++ src/main/java/org/stax0o/project/hotelifybackend/dto/BookingDTO.java
@@ -1,9 +1,30 @@
 package org.stax0o.project.hotelifybackend.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import org.stax0o.project.hotelifybackend.enums.PaymentStatus;
 
 import java.time.LocalDate;
 
-public record BookingDTO(Long id, Long userId, Long roomId, LocalDate startDate, LocalDate endDate, Double cost,
-                         PaymentStatus paymentStatus) {
+public record BookingDTO(
+        Long id,
+
+        Long userId,
+
+        Long roomId,
+
+        @NotNull
+        @FutureOrPresent(message = "Дата должна быть текущая или будущая")
+        LocalDate startDate,
+
+        @NotNull
+        @FutureOrPresent(message = "Дата должна быть текущая или будущая")
+        LocalDate endDate,
+
+        @NotNull
+        @DecimalMin(value = "1.0", message = "Стоимость должна быть положительной")
+        Double cost,
+
+        PaymentStatus paymentStatus) {
 }
--- src/main/java/org/stax0o/project/hotelifybackend/entity/Room.java
+++ src/main/java/org/stax0o/project/hotelifybackend/entity/Room.java
@@ -18,7 +18,7 @@ public class Room {
     private String name;
 
     @ManyToOne
-    @JoinColumn(name = "hotel_id")
+    @JoinColumn(name = "hotel_id", nullable = false)
     private Hotel hotel;
 
     @Column(nullable = false)
